879188d3dbb5ad7c453656ee9176843a4be5bec1
Jahpods <aculturedman45@gmail.com>
2023-03-20 12:55:12 +1000

    more stuff

--- Cycle 1 Assignment 2D/Assets/Script/EnemySpawner.cs
+++ Cycle 1 Assignment 2D/Assets/Script/EnemySpawner.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float enemyInterval = 10f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(enemyInterval, enemyPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+
     }
 
     // Update is called once per frame
